e1c0663669a8c3c7ed9bbf6c9cc3167b3f663008
NRE <nicolasreaud@gmail.com>
2019-04-17 21:13:16 +0200

    NRE evos remises

---
+++ Assets/chickenController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chickenController : MonoBehaviour
+{
+    bool begin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //When button "PlayButton" change of state -> begin to walk
+}
