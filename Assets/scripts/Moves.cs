1783e4064cc0489e1b2eabf375db37f92c098408
Echo <alexis.martial16@gmail.com>
2019-04-17 18:06:53 +0200

    alexis il fait des trucs
    les poules et les sols

---
+++ Assets/scripts/Moves.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moves : MonoBehaviour {
+	private Rigidbody2D physic;
+	private States state;
+	private Transform transform;
+
+	public float acceleration;
+	public float speedNormal;
+	public float speedZombie;
+	public float speedFeu;
+	private float m_maxSpeed;
+	// Use this for initialization
+	void Start () {
+		physic = gameObject.GetComponent<Rigidbody2D> ();
+		state = gameObject.GetComponent<States>();
+		transform = gameObject.GetComponent<Transform> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void FixedUpdate(){
+
+		if (Mathf.Abs (physic.velocity.x) < m_maxSpeed) {
+			physic.AddForce (Vector2.right * acceleration * state.m_direction);
+		}
+	}
+}
---
+++ Assets/scripts/States.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class States : MonoBehaviour {
+
+	public enum Etat {normal, glace, feu, zombie};
+	public enum Type {poussin, poule, coq, super};
+
+	//States
+	public int m_direction = 0;//left -1, right 1, else 0
+	public Etat m_etat = Etat.normal;
+	public Type m_type = Type.poussin;
+	// Use this for initialization
+
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
