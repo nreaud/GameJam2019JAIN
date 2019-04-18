cc4d53a0ad40b0f10687f9a07606d586a058b5e0
LamiJon <zouzmania@gmail.com>
2019-04-18 17:09:45 +0200

    feu + glace

---
+++ Assets/scripts/Burns.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Burns : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	void OnTriggerExit2D(Collider2D hit)
+	{
+		if(hit.gameObject.tag == "poulet")
+		{
+			if(hit.gameObject.GetComponent<States>().m_etat == States.Etat.normal)
+			{
+				hit.gameObject.GetComponent<States>().m_etat = States.Etat.feu;
+			}
+			else if (hit.gameObject.GetComponent<States>().m_etat == States.Etat.glace)
+			{
+				hit.gameObject.GetComponent<States>().m_etat = States.Etat.normal;
+			}
+			else if (hit.gameObject.GetComponent<States>().m_etat == States.Etat.zombie)
+			{
+				hit.gameObject.GetComponent<States>().m_etat = States.Etat.mort;
+			}
+		}
+	}
+}
---
+++ Assets/scripts/Freezes.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Freezes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	void OnTriggerExit2D(Collider2D hit)
+	{
+		if(hit.gameObject.tag == "poulet")
+		{
+			if(hit.gameObject.GetComponent<States>().m_etat == States.Etat.normal)
+			{
+				hit.gameObject.GetComponent<States>().m_etat = States.Etat.glace;
+			}
+			else if (hit.gameObject.GetComponent<States>().m_etat == States.Etat.feu)
+			{
+				hit.gameObject.GetComponent<States>().m_etat = States.Etat.normal;
+			}
+			else if (hit.gameObject.GetComponent<States>().m_etat == States.Etat.zombie)
+			{
+				hit.gameObject.GetComponent<States>().m_etat = States.Etat.mort;
+			}
+		}
+	}
+}
