5a2e3d0dfe936aa1ae666bd69d05bbc8c33d45c9
NRE <nicolasreaud@gmail.com>
2019-04-18 15:57:16 +0200

    PlayButton working! youhou

---
+++ Assets/scripts/StartButtonController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButtonController : MonoBehaviour
+{
+    public bool start;
+    public Object[] poulets;
+    // Start is called before the first frame update
+    void Start()
+    {
+        start = false;   
+    }   
+
+    private void OnMouseDown()
+    {
+        start = true;
+        GameObject[] chickens = GameObject.FindGameObjectsWithTag("Chicken");
+        Debug.Log("Chickens: " + chickens);
+        foreach (GameObject chicken in chickens){
+            Debug.Log("Chicken: " + chicken);
+            chicken.GetComponent<Moves>().enabled = true;
+        }
+
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("Object");
+        Debug.Log("Objects: " + objects);
+        foreach (GameObject o in objects)
+        {
+            Debug.Log("Object: " + o);
+            o.GetComponent<ObjectController>().drageable = false;
+        }
+    }
+
+
+}
--- Assets/scripts/objectController.cs
+++ Assets/scripts/objectController.cs
@@ -2,16 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class objectController : MonoBehaviour
-{ 
+public class ObjectController : MonoBehaviour
+{
+    public bool drageable;
+
+    void Start()
+    {
+        drageable = true;
+    }
+
     private void OnMouseDrag()
     {
-        //When mousePos will be translated into world point it will keep z pos of object unchanged
-        float fixedZPos = transform.position.z - Camera.main.transform.position.z;
-        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, fixedZPos);
-        //Mouse Position has to be translated into world point
-        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        transform.position = objPosition;
+        if (drageable)
+        {
+            //When mousePos will be translated into world point it will keep z pos of object unchanged
+            float fixedZPos = transform.position.z - Camera.main.transform.position.z;
+            Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, fixedZPos);
+            //Mouse Position has to be translated into world point
+            Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            transform.position = objPosition;
+        }
     }
 
     //When playButton change state -> cannot be moved any longer + gain rigidbody (at the start doesnt have one)
