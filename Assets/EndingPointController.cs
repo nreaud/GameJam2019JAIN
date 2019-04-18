743e6f8fb214674bc7eb4dabe96856efb868838b
NRE <nicolasreaud@gmail.com>
2019-04-18 17:39:07 +0200

    Start + endPoint waouh!

---
+++ Assets/EndingPointController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndingPointController : MonoBehaviour
+{
+
+    public Text winText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        winText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        winText.text = "Ez Win GG my man!";
+    }
+}
